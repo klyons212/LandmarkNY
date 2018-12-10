c7bb0921fc1124762f886fe27e8f66a5fc5e318d
Karam Lyons <KLyons@Karams-MacBook-Pro.local>
2018-12-09 22:22:16 -0500

    tourpicker

---
+++ LandmarkNY/TourPickerViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TourPickerViewController.swift
+//  LandmarkNY
+//
+//  Created by Karam Lyons on 12/9/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+
+class TourPickerViewController: UIViewController{
+    
+    @IBOutlet weak var tour1: UICollectionView!
+    @IBOutlet weak var tour2: UICollectionView!
+    @IBOutlet weak var tour3: UICollectionView!
+    
+    
+    
+    
+    
+}
